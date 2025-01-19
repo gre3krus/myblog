4ff30dc4a431f3d5a56f606d9502f5d0db01b642
VladR <vladi.rolan@gmail.com>
2025-01-19 21:02:44 +0300

    Реализовать шапку

---
+++ src/components/header/components/logo/logo.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import { Icon } from '../../../../components';
+import styled from 'styled-components';
+
+const LargeText = styled.div`
+	font-size: 48px;
+	font-weight: 600;
+`;
+
+const SmallText = styled.div`
+	font-size: 18px;
+	font-weight: bold;
+`;
+
+const LogoContainer = ({ className }) => {
+	return (
+		<Link className={className} to="/">
+			<Icon id="fa-code" size="70px" />
+			<div>
+				<LargeText>Блог</LargeText>
+				<SmallText>веб-разработчика</SmallText>
+			</div>
+		</Link>
+	);
+};
+
+export const Logo = styled(LogoContainer)`
+	display: flex;
+	gap: 9px;
+	line-height: 40px;
+`;
